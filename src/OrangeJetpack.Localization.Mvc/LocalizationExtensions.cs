5f306bce9ecfbf8ccf45a110728e37884f382ee6
Andy Mehalick <andy.mehalick@orangejetpack.com>
2016-07-22 19:19:01 +0300

    added check on model state before trying to clear errors

--- src/OrangeJetpack.Localization.Mvc/LocalizationExtensions.cs
+++ src/OrangeJetpack.Localization.Mvc/LocalizationExtensions.cs
@@ -8,7 +8,7 @@ namespace OrangeJetpack.Localization.Mvc
 {
     public static class LocalizationExtensions
     {
-        public static T Update<T>(this T item, ModelStateDictionary modelState, Expression<Func<T, string>> property, LocalizedContent[] contents) where T : class, ILocalizable
+        public static T Update<T>(this T item, ModelStateDictionary modelStateDictionary, Expression<Func<T, string>> property, LocalizedContent[] contents) where T : class, ILocalizable
         {
             item.Set(property, contents);
 
@@ -29,11 +29,19 @@ namespace OrangeJetpack.Localization.Mvc
             {
                 var memberExpression = (MemberExpression)property.Body;
                 var propertyInfo = (PropertyInfo)memberExpression.Member;
+                var propertyName = propertyInfo.Name;
 
-                modelState[propertyInfo.Name].Errors.Clear();
+                var modelState = modelStateDictionary
+                    .Where(i => i.Key.EndsWith(propertyName))
+                    .Where(i => i.Value.Errors.Any())
+                    .Select(i => i.Value)
+                    .FirstOrDefault();
+
+                modelState?.Errors.Clear();
             }
 
             return item;
         }
     }
+}
 }
